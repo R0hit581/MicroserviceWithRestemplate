0c9fac0d842303abe89267f9d7ed590b40dead46
R0hit <rohitkushwaha581@gmail.com>
2025-04-24 16:49:18 +0530

    added code

---
+++ ProductService/src/main/java/com/product/entity/Product.java
@@ -0,0 +1,19 @@
+package com.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+	@Id
+	private int productId;
+	private String productName;
+	private int productQty;
+}
