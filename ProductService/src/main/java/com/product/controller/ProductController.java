250cbd13ac061fc222c7ff783e0b16f87b4771fd
R0hit <rohitkushwaha581@gmail.com>
2025-04-24 17:48:23 +0530

    added changes

--- ProductService/src/main/java/com/product/controller/ProductController.java
+++ ProductService/src/main/java/com/product/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.product.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,17 @@ public class ProductController {
 	@PostMapping("/add")
 	public ResponseEntity<?> addProduct(@RequestBody Product product){
 		return productService.add(product);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteProduct(@PathVariable("id") int id) {
+		System.out.println(id);
+		 productService.delete(id);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Product>> getAllProduct(){
+		return productService.getAllProduct();
 	}
 }
 
--- ProductService/src/main/java/com/product/service/ProductService.java
+++ ProductService/src/main/java/com/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.product.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,20 @@ public class ProductService {
 
 	public ResponseEntity<?> add(Product product) {
 		// TODO Auto-generated method stub
+		product.setProductAvailable(product.getProductQty()-product.getProductSold());
 		Product p= productRepository.save(product);
 		return ResponseEntity.ok().body(p);
+	}
+
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		productRepository.deleteById(id);
+	}
+
+	public ResponseEntity<List<Product>> getAllProduct() {
+		// TODO Auto-generated method stub
+		List<Product> prodList= productRepository.findAll();
+		return ResponseEntity.ok(prodList);
 	}
 
 }
