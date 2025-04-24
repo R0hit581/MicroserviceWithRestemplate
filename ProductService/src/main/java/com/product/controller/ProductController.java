7ddebf05a960673b5d5ab1aa1edc389c11708edc
R0hit <rohitkushwaha581@gmail.com>
2025-04-24 17:13:34 +0530

    added more code

--- ProductService/src/main/java/com/product/controller/ProductController.java
+++ ProductService/src/main/java/com/product/controller/ProductController.java
@@ -1,10 +1,28 @@
 package com.product.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.product.entity.Product;
+import com.product.service.ProductService;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+	private final ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		// TODO Auto-generated constructor stub
+		this.productService=productService;
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<?> addProduct(@RequestBody Product product){
+		return productService.add(product);
+	}
 }
+
--- ProductService/src/main/java/com/product/service/ProductService.java
+++ ProductService/src/main/java/com/product/service/ProductService.java
@@ -1,8 +1,24 @@
 package com.product.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.product.entity.Product;
+import com.product.repository.ProductRepository;
 
 @Service
 public class ProductService {
+	
+	private final ProductRepository productRepository;
+	
+	ProductService(ProductRepository productRepository){
+		this.productRepository=productRepository;
+	}
+
+	public ResponseEntity<?> add(Product product) {
+		// TODO Auto-generated method stub
+		Product p= productRepository.save(product);
+		return ResponseEntity.ok().body(p);
+	}
 
 }
